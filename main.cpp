ee6af7ddcee9fa3a5b880229de7aafdc8f6c2a3b
adrianram12 <adrianramos2121@gmail.com>
2022-10-29 13:18:09 -0700

    testing solvePuzzle function

--- main.cpp
+++ main.cpp
@@ -10,11 +10,13 @@ using namespace std;
 
 vector<vector<int> > level1 = {{1,2,3}, {4,5,6}, {7,8,0}};
 vector<vector<int> > level2 = {{1,2,3}, {4,5,6}, {0,7,8}};
+vector<vector<int> > level3 = {{1,2,3}, {5,0,6}, {4,7,8}};
+vector<vector<int> > level4 = {{1,3,6}, {5,0,2}, {4,7,8}};
 
 
 int main(){
 
-  BoardState* theInitial = new BoardState(level2);
+  BoardState* theInitial = new BoardState(level1);
   expandStates.push(theInitial);
 
   //theInitial.printBoardState();
